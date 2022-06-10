5b0ee03e9864b8fad1e5abbe085340aa18cff1a0
冯 <79425603+ffha@users.noreply.github.com>
2022-06-10 15:48:42 +0800

    Update start.sh

--- code/start.sh
+++ code/start.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set +e
-echo "Installing PHP 8.1"
-curl -sSL https://packages.sury.org/php/README.txt | sudo bash -x
-apt update
-apt install php8.1 php8.1-fpm php8.1-mysql php8.1-imagick php8.1-gd php8.1-mbstring -y
+
 mkdir -p /tmp/log/nginx/
 mkdir -p /tmp/var/nginx/
 mkdir -p /tmp/var/sessions/
@@ -12,6 +9,11 @@ mkdir -p /mnt/auto/typecho/sessions
 chown -R www-data:www-data /mnt/auto/typecho/sessions
 
 chown -R www-data:www-data /mnt/auto/typecho
+
+echo "Installing PHP 8.1"
+curl -sSL https://packages.sury.org/php/README.txt | sudo bash -x
+apt update
+apt install --no-install-recommends php8.1 php8.1-fpm php8.1-mysql php8.1-imagick php8.1-gd php8.1-mbstring -y
 
 echo "start php-fpm"
 php-fpm8.1 -c /code/php.ini -y /code/php-fpm.conf
